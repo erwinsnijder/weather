31dd64db1f1762ad52f4c135cef197a58d518694
erwin snijder <erwinsnijder_4@hotmail.com>
2019-06-26 14:47:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let appID = "4d34983a5595bd557e758143da95a384";
 let units = "metric";
-let searchMethod ="q";
+let searchMethod;
+
+function getSearchMethod(searchTerm) {
+if(searchTerm.length == 5 && Number.parseInt(searchTerm) + "" == searchTerm)
+searchMethod = "zip";
+else 
+searchMethod = "q";
+}
+
 
 function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
     fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appID}&units=${units}`).then(result => {
     return result.json();
 
