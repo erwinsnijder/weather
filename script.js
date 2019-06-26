0aa709cd63ae005b9ba226759adf03e600356eee
erwin snijder <erwinsnijder_4@hotmail.com>
2019-06-26 11:55:57 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-$.GetJSON("https://api.openweathermap.org/data/2.5/weather?q=Groningen&units=metric&APPID=4d34983a5595bd557e758143da95a384", function(data){
-console.log(data);
-});
+let appID = "4d34983a5595bd557e758143da95a384";
+let units = "metric";
+let searchMethod;
+
+function searchWeather(searchTerm) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appID}&units=${units}`).then(result => {
+    return result.json();
+
+    }).then(result => {
+        
+    })
+}
